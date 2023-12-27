a0a9957ee9e7e51978dcc81e7f86067fba5f85a8
tema5002 <152618947+tema5002@users.noreply.github.com>
2023-12-27 19:15:02 +0300

    amigger and his amigger :baby_chick:

---
+++ SAVE AMIGGER QUOTES.py
@@ -0,0 +1,32 @@
+import pickle
+
+amiggerquotes=[
+    "amigger and his family :joy::joy::joy:",
+    "amigger and his family :joy::joy::grinning:",
+    "family and his amigger :hedgehog::evergreen_tree::frog:",
+    "amigger and his bushes",
+    "amigger and his :bat::bat::bat::bat:",
+    "amigger ate a lot of people :rolling_eyes:",
+    "amigger is melting tigger5002's niggtop with 150ns pnig",
+    "amigger and his son is my password :sob::key:",
+    "amigger and his son was supposed be a woman :female_sign:",
+    "amigger and his wife are my favorite songs about this one :clock130::heart_eyes::heart::grin::smile::smiley_cat:",
+    "amigger and I will probably leave my office today :grin:",
+    "amigger and his family and I can do the earthquake :shaking_face::cold_face::shaking_face::shaking_face:",
+    "amigger and I can get rid the body :eye::tongue::eye:",
+    "amigger and his daughter :laughing:",
+    "amigger and his compression :compression:",
+    "amigger and his amigger :baby_chick:",
+    "amigger and flowmigger eating and drinking water :sweat_drops::joy::blush::wink::grin::slight_smile:",
+    "amigger and his children :pie:",
+    "amigger and the void :space_invader:",
+    "amigger and his cell machine :microbe:",
+    "amigger and he killed himself :joy::joy:",
+    "amigger is a good :blush::relieved:",
+    "amigger and his cell machine\nthe game is controller by ammeter and not you",
+    "does amigger having a family",
+    "hey amigger add a few minutes ago to your house :house_with_garden::wave::house:",
+    "hey amigger and then I got the super intense for a few weeks and the :evergreen_tree::hedgehog::evergreen_tree::hedgehog:"
+]
+
+pickle.dump(amiggerquotes, open("amiggerquotes.dat", "wb"))
